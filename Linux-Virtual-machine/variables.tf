e6666d888d720ee8032ef85c706d8c15e9bc059a
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-02-28 09:54:14 +1300

    Update variables.tf

--- Linux-Virtual-machine/variables.tf
+++ Linux-Virtual-machine/variables.tf
@@ -1,64 +1,51 @@
 variable "location" {
   type        = string
-  description = "Azure region"
+  description = "Azure region for all resources."
   default     = "australiaeast"
 }
 
 variable "resource_group_name" {
   type        = string
-  description = "Resource group name"
+  description = "Name of the Resource Group to create/use."
 }
 
 variable "vnet_name" {
   type        = string
-  description = "VNet name"
+  description = "Name of the Virtual Network."
 }
 
 variable "vnet_cidr" {
   type        = string
-  description = "VNet CIDR"
+  description = "CIDR for the Virtual Network."
   default     = "10.10.0.0/16"
 }
 
 variable "subnet_name" {
   type        = string
-  description = "Subnet name"
+  description = "Name of the Subnet."
 }
 
 variable "subnet_cidr" {
   type        = string
-  description = "Subnet CIDR"
+  description = "CIDR for the Subnet."
   default     = "10.10.1.0/24"
 }
 
 variable "vm_name" {
   type        = string
-  description = "Base VM name"
+  description = "Base VM name. You can suffix/index this in resources."
   default     = "vm-devops-01"
 }
 
 variable "vm_size" {
   type        = string
-  description = "VM size"
+  description = "Azure VM size."
   default     = "Standard_B2s"
 }
 
 variable "admin_username" {
   type        = string
-  description = "Admin username"
+  description = "Admin username for SSH."
   default     = "azureuser"
-}
-
-# Optional: if empty, ssh.tf will generate a key pair and use its public key
-variable "ssh_public_key" {
-  type        = string
-  description = "Optional OpenSSH public key string"
-  default     = ""
-}
-
-variable "prefix" {
-  type        = string
-  description = "Name prefix used for resource names (VM/NIC/NSG/PIP/etc.)"
-  default     = "devops-lab"
 }
 
