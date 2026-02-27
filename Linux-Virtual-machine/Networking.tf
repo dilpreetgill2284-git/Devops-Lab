2ad7f92cbdaae6bdba0ceb70619594db145890d5
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-02-27 21:48:59 +0000

    Fix: correct RG data source refs in Networking.tf (data.azurerm_resource_group)

--- Linux-Virtual-machine/Networking.tf
+++ Linux-Virtual-machine/Networking.tf
@@ -2,14 +2,14 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-network"
   address_space       = var.vnet_address_space
-  location            = data.data.azurerm_resource_group.rg.location
-  resource_group_name = data.data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 # Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  resource_group_name  = data.data.azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_cidr]
 }
@@ -18,8 +18,8 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_network_interface" "nic" {
   for_each            = var.vm_names
   name                = "${var.prefix}-nic-${each.key}"
-  location            = data.data.azurerm_resource_group.rg.location
-  resource_group_name = data.data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "ipconfig1"
