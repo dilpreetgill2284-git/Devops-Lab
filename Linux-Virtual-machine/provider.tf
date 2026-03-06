b23c5252fdee9012b71c274e2f71b095cb88a304
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-06 15:59:20 +1300

    Update provider.tf

--- Linux-Virtual-machine/provider.tf
+++ Linux-Virtual-machine/provider.tf
@@ -1,14 +1,8 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
+      version = "~> 3.113"
     }
   }
 }
