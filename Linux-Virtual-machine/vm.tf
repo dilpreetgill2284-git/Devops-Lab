6b1ba2e6963503eb3ae092d71900a4cfb637bdf4
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-06 16:05:34 +1300

    Update vm.tf

--- Linux-Virtual-machine/vm.tf
+++ Linux-Virtual-machine/vm.tf
@@ -1,18 +1,21 @@
 resource "azurerm_linux_virtual_machine" "vm" {
   for_each            = var.vm_names
   name                = "${var.prefix}-${each.key}"
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
   size                = var.vm_size
   admin_username      = var.admin_username
-  tags                = var.tags
 
   network_interface_ids = [
-    azurerm_network_interface.nic[each.key].id
+    azurerm_network_interface.nic[each.key].id,
   ]
 
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+
   os_disk {
-    name                 = "${var.prefix}-${each.key}-osdisk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -20,14 +23,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    sku       = "22_04-lts"
     version   = "latest"
   }
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = local.effective_public_key
-  }
-
-  disable_password_authentication = true
 }
