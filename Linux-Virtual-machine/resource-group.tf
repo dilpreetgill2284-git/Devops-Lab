56fd43fc3aaa0d33b2bbde3d2f39415c7d57f401
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-06 15:59:39 +1300

    Update resource-group.tf

--- Linux-Virtual-machine/resource-group.tf
+++ Linux-Virtual-machine/resource-group.tf
@@ -1,3 +1,4 @@
-data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-resources"
+  location = var.location
 }
