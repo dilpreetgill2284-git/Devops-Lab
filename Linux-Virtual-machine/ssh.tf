1987ab0f179b47cc9bfe957a9f8c5eeb10cdbbc0
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-06 22:24:51 +1300

    Delete Linux-Virtual-machine/ssh.tf

--- Linux-Virtual-machine/ssh.tf
+++
@@ -1,8 +0,0 @@
-
-# We do not generate keys in Terraform. 
-# CI creates the public key file and we read it from var.ssh_public_key_path.
-
-output "ssh_public_key_effective" {
-  value       = file(var.ssh_public_key_path)
-  description = "Public key used for the VMs (read from the path provided)"
-}
