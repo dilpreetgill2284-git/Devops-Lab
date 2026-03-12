fca7bbdddc7435842d504cda7d0b2c5571bf68bb
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-13 11:33:08 +1300

    Update provider.tf

--- Linux-Virtual-machine/azure/provider.tf
+++ Linux-Virtual-machine/azure/provider.tf
@@ -4,27 +4,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.113"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.13"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.27"
-    }
-  }
 }
 
 provider "azurerm" {
   features {}
 }
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
 }
