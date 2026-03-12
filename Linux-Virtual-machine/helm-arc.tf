41ec96e15ff0bb6a66a3ce9cf5ff25b7c6fa2c71
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-13 09:53:58 +1300

    Update helm-arc.tf

--- Linux-Virtual-machine/helm-arc.tf
+++ Linux-Virtual-machine/helm-arc.tf
@@ -7,11 +7,11 @@ resource "helm_release" "arc" {
   # pick a recent stable version; keep it pinned to avoid surprises
   version    = "0.23.6"
 
-  create_namespace = true
-
   # If you do NOT want ARC to create an empty auth secret on its own:
   # (In your manual setup you created the controller-manager secret yourself.)
-  set {
+  # We manage the auth secret ourselves (lab: PAT)
+
+ set {
     name  = "authSecret.create"
     value = "false"
   }
