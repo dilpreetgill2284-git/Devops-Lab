db92393f2963e9170955bbf4185f592cc30e13be
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-13 16:14:48 +1300

    Update aks.tf

--- AKS-Azure/aks.tf
+++ AKS-Azure/aks.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "system"
     node_count = 1
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_B2als_v2"
   }
 
   identity {
