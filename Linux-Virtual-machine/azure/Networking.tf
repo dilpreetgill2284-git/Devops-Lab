d1b4d2f624501def31ff2985bb021f168f7e89dd
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-14 14:48:57 +1300

    Update Networking.tf

--- Linux-Virtual-machine/azure/Networking.tf
+++ Linux-Virtual-machine/azure/Networking.tf
@@ -16,7 +16,7 @@ resource "azurerm_subnet" "subnet" {
 
 # Network Interface
 resource "azurerm_network_interface" "nic" {
-  for_each            = var.vm_names
+  for_each            = local.vm_set
   name                = "${var.prefix}-nic-${each.key}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
