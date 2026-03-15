123c3ea2d1f2732a9cf66fd79147783572a4326b
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-16 10:00:01 +1300

    Create runner.tf

---
+++ AKS-Azure/runner.tf
@@ -0,0 +1,19 @@
+resource "kubernetes_manifest" "repo_runner" {
+  manifest = {
+    apiVersion = "actions.summerwind.dev/v1alpha1"
+    kind       = "RunnerDeployment"
+    metadata = {
+      name      = "repo-runner"
+      namespace = "actions-runner-system"
+    }
+    spec = {
+      replicas = 1
+      template = {
+        spec = {
+          repository = "dilpreetgill2284-git/Devops-Lab"
+          labels     = ["self-hosted", "aks"]
+        }
+      }
+    }
+  }
+}
