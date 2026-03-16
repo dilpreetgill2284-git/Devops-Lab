6920e27ff35f29eede592ea8364692e438f28dc5
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-16 20:45:30 +1300

    Update arc.tf

--- AKS-Platform-Core/arc.tf
+++ AKS-Platform-Core/arc.tf
@@ -1,18 +1,20 @@
 resource "helm_release" "arc" {
 
-  depends_on = [
-    helm_release.arc_crds
-  ]
-
   name             = "arc"
   namespace        = "actions-runner-system"
   create_namespace = true
 
   repository = "https://actions-runner-controller.github.io/actions-runner-controller"
   chart      = "actions-runner-controller"
+  version    = "0.23.7"
 
   set {
     name  = "authSecret.create"
     value = "false"
+  }
+
+  set {
+    name  = "installCRDs"
+    value = "true"
   }
 }
