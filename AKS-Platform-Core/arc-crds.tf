feda1dd7613203037b1d4515d86d3770679bf0bd
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-16 20:46:21 +1300

    Delete AKS-Platform-Core/arc-crds.tf

--- AKS-Platform-Core/arc-crds.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "arc_crds" {
-  name             = "arc-crds"
-  namespace        = "actions-runner-system"
-  create_namespace = true
-
-  repository = "https://actions-runner-controller.github.io/actions-runner-controller"
-  chart      = "actions-runner-controller-crds"
-}
