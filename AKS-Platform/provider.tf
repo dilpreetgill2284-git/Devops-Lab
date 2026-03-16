d3caee6b4b473d73fd96b476ef6b5b2e2406ac74
dilpreetgill2284-git <dilpreet.gill2284@gmail.com>
2026-03-16 17:52:53 +1300

    Create provider.tf

---
+++ AKS-Platform/provider.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "kubernetes" {
+
+  host = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
+
+  client_certificate = base64decode(
+    data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  )
+
+  client_key = base64decode(
+    data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+  )
+
+  cluster_ca_certificate = base64decode(
+    data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+  )
+
+}
+
+provider "helm" {
+
+  kubernetes {
+
+    host = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
+
+    client_certificate = base64decode(
+      data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+    )
+
+    client_key = base64decode(
+      data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+    )
+
+    cluster_ca_certificate = base64decode(
+      data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+    )
+
+  }
+
+}
